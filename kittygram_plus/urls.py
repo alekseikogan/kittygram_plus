68f6ddd707fda5fff53e6a284be0a679c80ec5dc
Алексей Коган <alekseikogan@yandex.ru>
2022-11-24 21:56:41 +0300

    добавил owner, achivments

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,11 +2,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet
 
 
 router = DefaultRouter()
-router.register('cats', CatViewSet)
+router.register('cats', CatViewSet, basename='cats')
+router.register('owners', OwnerViewSet, basename='owners')
 
 urlpatterns = [
     path('', include(router.urls)),
