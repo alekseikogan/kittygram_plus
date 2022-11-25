2755795423cf3a1d025b3948ced204d23f0bafb6
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2022-11-25 17:03:57 +0300

    some fixes

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
+from .models import AchievementCat, Cat, Owner, Achievement, CHOICES
 
-from .models import AchievementCat, Cat, Owner, Achievement
+
+class Hex2NameColor(serializers.Field):
+
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
 
 class AchievementSerializer(serializers.ModelSerializer):
-
+    achievement_name = serializers.CharField(source='name')
+    
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -18,12 +36,14 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 
 class CatSerializer(serializers.ModelSerializer):
+    age = serializers.SerializerMethodField()
     achievements = AchievementSerializer(many=True, required=False)
-    
+    color = serializers.ChoiceField(choices=CHOICES)
+
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
-    
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:
             cat = Cat.objects.create(**validated_data)
@@ -38,3 +58,6 @@ class CatSerializer(serializers.ModelSerializer):
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
